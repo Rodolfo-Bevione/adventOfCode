03cfb143e26c6e1289082cec4092b0c8ba0dae3f
Rodolfo Bevione <rodolfo.bevione@gmail.com>
2024-12-07 19:05:08 +0100

    using maps

--- 01-12-2024/src/main/part2.go
+++ 01-12-2024/src/main/part2.go
@@ -4,20 +4,19 @@ func computePartTwo(first, second []int) int {
 	if len(first) != len(second) {
 		panic("Le lunghezze sono diverse!")
 	}
+	secondArrayMapped := make(map[int]int)
 
-	// run through every element of array A
-	// if element X appears in list B, increment a counter
-	// At the end, multiply X by the counter and increase the total score
+	for _, elementFromSecondArray := range second {
+		howMany := secondArrayMapped[elementFromSecondArray]
+
+		secondArrayMapped[elementFromSecondArray] = howMany + 1
+	}
+
 	result := 0
 	for i := 0; i < len(first); i++ {
 		elementFromFirstArray := first[i]
-		howMany := 0
-		for _, elementFromSecondArray := range second {
-			if elementFromFirstArray == elementFromSecondArray {
-				howMany++
-			}
-		}
-		result = result + (howMany * elementFromFirstArray)
+
+		result = result + (elementFromFirstArray * secondArrayMapped[elementFromFirstArray])
 	}
 	return result
 }
